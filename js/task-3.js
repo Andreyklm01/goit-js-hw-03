d31133d9559d98bb16c9fc6ee353aa1a08e8d970
Andrey Klemenko <web.klemenko@gmail.com>
2020-12-13 01:50:45 +0200

    complete tasks 3,4. Create task 5.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,37 @@
-const findBestEmployee = function (employees) {};
+// Напиши функцию findBestEmployee(employees), которая принимает объект сотрудников и возвращает
+// имя самого продуктивного(который выполнил больше всех задач).
+// Сотрудники и кол - во выполненых задач содержатся как свойства объекта в формате"имя": "кол-во задач".
+
+// алгоритм:
+// функция вытягивает значения и ключи
+// ищет самое большое число в значениях maxValue.
+// находит индекс maxValue
+// выводит ключ у которого есть значение maxValue.
+
+const findBestEmployee = function (employees) {
+  const bestEmployee = Object.keys(employees);
+  const tasks = Object.values(employees);
+
+  const maxValue = Math.max(...tasks);
+  const indexMaxValue = tasks.indexOf(maxValue);
+
+  return bestEmployee[indexMaxValue];
+};
+
+// способ 2
+// const findBestEmployee = function (employees) {
+//   let bestEmployee;
+//   let maxTasks = 0;
+
+//   for (const employee in employees) {
+//     const tasks = employees[employee];
+//     if (tasks > maxTasks) {
+//       maxTasks = tasks;
+//       bestEmployee = employee;
+//     }
+//   }
+//   return bestEmployee;
+// };
 
 /*
  * Вызовы функции для проверки работоспособности твоей реализации.
@@ -12,19 +45,19 @@ console.log(
   }),
 ); // lorence
 
-// console.log(
-//   findBestEmployee({
-//     poly: 12,
-//     mango: 17,
-//     ajax: 4,
-//   }),
-// ); // mango
+console.log(
+  findBestEmployee({
+    poly: 12,
+    mango: 17,
+    ajax: 4,
+  }),
+); // mango
 
-// console.log(
-//   findBestEmployee({
-//     lux: 147,
-//     david: 21,
-//     kiwi: 19,
-//     chelsy: 38,
-//   }),
-// ); // lux
+console.log(
+  findBestEmployee({
+    lux: 147,
+    david: 21,
+    kiwi: 19,
+    chelsy: 38,
+  }),
+); // lux
